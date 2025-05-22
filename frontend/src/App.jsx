ea393777e172f44ba5a2e9997087a351877a9af0
Olaco_IT2R4 <olacomarkaidel@gmail.com>
2025-05-22 12:39:53 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MainPage from './pages/Main/Main_page';
+
+<Route path="/Main" element={<MainPage />} />
+
 
 function App() {
   const [count, setCount] = useState(0)
